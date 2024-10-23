e1079208906a40c90e6a0b77818f0a041fa9abaa
cornelius31415 <cornelius@MBPvonCornelius.fritz.box>
2024-10-23 14:41:50 +0200

    added the BMP280 sensor for pressure, altitude and temperature measurement. also finally drew the circuit diagram for the prototype (on a breadboard).

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -8,7 +8,8 @@
 #include <SD.h>
 #include <Wire.h>
 #include <DS3231.h>
-
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
 // ----------------------------------------------------------------------------------------
 //                                  HARDWARE SETUP
 // ----------------------------------------------------------------------------------------
@@ -19,8 +20,10 @@ const int dht_type = DHT11; // type of DHT sensor
 const int SD_pin = 10;      // Arduino Pin for CS Pin of SD card module
 
 float humidity;             // the shit I wanna measure
-float temperature;
-
+float temperatureDHT;
+float temperatureBMP;
+float pressure;
+float altitude;
 // ----------------------------------------------------------------------------------------
 //                                  CREATING OBJECTS
 // ----------------------------------------------------------------------------------------
@@ -28,13 +31,13 @@ float temperature;
 DHT dht(dht_pin, dht_type); // creating a DHT object
 DS3231 clock;
 RTCDateTime dt;
-
+Adafruit_BMP280 bmp;
 
 void setup() {
 
-Serial.begin(9600);
 dht.begin();
 clock.begin();
+bmp.begin(0x76);
 
 clock.setDateTime(__DATE__, __TIME__);
 
@@ -44,13 +47,21 @@ File weatherData = SD.open("weather.csv",FILE_WRITE);
 
 if(weatherData){                                    // check if file has been opened successfully
 
-weatherData.println("Time,Temperature,Humidity");   // print the header (column names) of the dataset
+weatherData.println("Time,Temperature DHT,Humidity DHT (%), Temperature BMP, Pressure BMP (hPa), Altitude BMP (m)");   // print the header (column names) of the dataset
 
 weatherData.close();                                // close the data set
 
 
 }
 
+
+
+//configuring the BMP280 sensor
+bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Normaler Modus
+                Adafruit_BMP280::SAMPLING_X2,     // Temperatur-Oversampling: x2
+                Adafruit_BMP280::SAMPLING_X16,    // Druck-Oversampling: x16
+                Adafruit_BMP280::FILTER_X16,      // Filter: x16
+                Adafruit_BMP280::STANDBY_MS_500); // Standby-Zeit: 500ms
 
 
 }
@@ -61,11 +72,16 @@ void loop() {
 dt = clock.getDateTime();
 
 humidity = dht.readHumidity();
-temperature = dht.readTemperature();
+temperatureDHT = dht.readTemperature();
+delay(100);
+temperatureBMP = bmp.readTemperature();
+pressure = bmp.readPressure()/100.0;
+altitude = bmp.readAltitude();
 
 File weatherData = SD.open("weather.csv",FILE_WRITE);
 
 if (weatherData){
+  
 weatherData.print(dt.year);
 weatherData.print("-");
 weatherData.print(dt.month <10? "0": "");
@@ -83,13 +99,21 @@ weatherData.print(":");
 weatherData.print(dt.second <10? "0": "");
 weatherData.print(dt.second);
 weatherData.print(",");
-weatherData.print(temperature);
+weatherData.print(temperatureDHT);
 weatherData.print(",");
-weatherData.println(humidity);
+weatherData.print(humidity);
+weatherData.print(",");
+weatherData.print(temperatureBMP);
+weatherData.print(",");  
+weatherData.print(pressure);
+weatherData.print(",");  
+weatherData.print(altitude);
+weatherData.println();
+ 
+
 weatherData.close();
-  
 }
 
-delay(5000);                                        // sensor is slow with measuring
+delay(2000);                                        // DHT11 sensor is slow with measuring
 
 }
