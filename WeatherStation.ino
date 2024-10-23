a3e35e73ae61a7ca5eef257b31102f2eac62ecdd
cornelius31415 <cornelius@MBPvonCornelius.fritz.box>
2024-10-23 15:26:30 +0200

    added a second BMP280 sensor, other I2C address needed for that.

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -21,9 +21,12 @@ const int SD_pin = 10;      // Arduino Pin for CS Pin of SD card module
 
 float humidity;             // the shit I wanna measure
 float temperatureDHT;
-float temperatureBMP;
-float pressure;
-float altitude;
+float temperatureBMP1;
+float pressure1;
+float altitude1;
+float temperatureBMP2;
+float pressure2;
+float altitude2;
 // ----------------------------------------------------------------------------------------
 //                                  CREATING OBJECTS
 // ----------------------------------------------------------------------------------------
@@ -31,13 +34,15 @@ float altitude;
 DHT dht(dht_pin, dht_type); // creating a DHT object
 DS3231 clock;
 RTCDateTime dt;
-Adafruit_BMP280 bmp;
+Adafruit_BMP280 bmp1;
+Adafruit_BMP280 bmp2;
 
 void setup() {
 
 dht.begin();
 clock.begin();
-bmp.begin(0x76);
+bmp1.begin(0x76);
+bmp2.begin(0x77);
 
 clock.setDateTime(__DATE__, __TIME__);
 
@@ -47,7 +52,8 @@ File weatherData = SD.open("weather.csv",FILE_WRITE);
 
 if(weatherData){                                    // check if file has been opened successfully
 
-weatherData.println("Time,Temperature DHT,Humidity DHT (%), Temperature BMP, Pressure BMP (hPa), Altitude BMP (m)");   // print the header (column names) of the dataset
+weatherData.println("Time,Temperature DHT,Humidity DHT (%), Temperature BMP1, Pressure BMP1 (hPa),"
+                    "Altitude BMP1 (m),Temperature BMP2, Pressure BMP2 (hPa), Altitude BMP2 (m)");   // print the header (column names) of the dataset
 
 weatherData.close();                                // close the data set
 
@@ -57,7 +63,13 @@ weatherData.close();                                // close the data set
 
 
 //configuring the BMP280 sensor
-bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Normaler Modus
+bmp1.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Normaler Modus
+                Adafruit_BMP280::SAMPLING_X2,     // Temperatur-Oversampling: x2
+                Adafruit_BMP280::SAMPLING_X16,    // Druck-Oversampling: x16
+                Adafruit_BMP280::FILTER_X16,      // Filter: x16
+                Adafruit_BMP280::STANDBY_MS_500); // Standby-Zeit: 500ms
+                
+bmp2.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Normaler Modus
                 Adafruit_BMP280::SAMPLING_X2,     // Temperatur-Oversampling: x2
                 Adafruit_BMP280::SAMPLING_X16,    // Druck-Oversampling: x16
                 Adafruit_BMP280::FILTER_X16,      // Filter: x16
@@ -74,14 +86,18 @@ dt = clock.getDateTime();
 humidity = dht.readHumidity();
 temperatureDHT = dht.readTemperature();
 delay(100);
-temperatureBMP = bmp.readTemperature();
-pressure = bmp.readPressure()/100.0;
-altitude = bmp.readAltitude();
+temperatureBMP1 = bmp1.readTemperature();
+pressure1 = bmp1.readPressure()/100.0;
+altitude1 = bmp1.readAltitude();
+temperatureBMP2 = bmp2.readTemperature();
+pressure2 = bmp2.readPressure()/100.0;
+altitude2 = bmp2.readAltitude();
 
 File weatherData = SD.open("weather.csv",FILE_WRITE);
 
 if (weatherData){
-  
+
+// data from the DS1307 time module
 weatherData.print(dt.year);
 weatherData.print("-");
 weatherData.print(dt.month <10? "0": "");
@@ -99,18 +115,32 @@ weatherData.print(":");
 weatherData.print(dt.second <10? "0": "");
 weatherData.print(dt.second);
 weatherData.print(",");
+
+// data from DHT11 sensor
 weatherData.print(temperatureDHT);
 weatherData.print(",");
 weatherData.print(humidity);
 weatherData.print(",");
-weatherData.print(temperatureBMP);
+
+// data from first BMP280 sensor
+weatherData.print(temperatureBMP1);
 weatherData.print(",");  
-weatherData.print(pressure);
+weatherData.print(pressure1);
 weatherData.print(",");  
-weatherData.print(altitude);
+weatherData.print(altitude1);
+weatherData.print(",");
+
+// data from second BMP280 sensor
+weatherData.print(temperatureBMP2);
+weatherData.print(",");  
+weatherData.print(pressure2);
+weatherData.print(",");  
+weatherData.print(altitude2);
+
+// finish the line
 weatherData.println();
  
-
+// and close the file
 weatherData.close();
 }
 
